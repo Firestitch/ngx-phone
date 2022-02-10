8c17570226e583bae5a1e66c0c07ad4f39ec93e2
Eugene Kubesh <codbasters@gmail.com>
2022-02-10 18:07:52 +0300

    export phone config

--- src/public_api.ts
+++ src/public_api.ts
@@ -15,3 +15,5 @@ export { PhoneMetadataService } from './app/services/phone-metadata.service';
 export { PhoneService } from './app/services/phone.service';
 export { IFsPhoneConfig, IPhoneConfig } from './app/interfaces/phone-config.interface';
 export { IFsPhoneValue } from './app/interfaces/phone-value.interface';
+
+export { FS_PHONE_CONFIG } from './app/providers/phone-config';
